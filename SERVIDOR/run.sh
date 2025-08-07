bc1728f2ba9990ebf92b78da0f61acd03b38480f
Jeremias Rodriguez <jeremiasrodriguez05@gmail.com>
2025-08-06 23:10:27 -0300

    Update run.sh

--- SERVIDOR/run.sh
+++ SERVIDOR/run.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+read -p "Ingrese el numero de PUERTO: " PORT
 make
-./servidorTSL 12345 ./certs/ca/ca_cert.pem ./certs/server/sv_cert.pem ./certs/server/private/sv_key.pem
+./servidorTSL "$PORT" ./certs/ca/ca_cert.pem ./certs/server/sv_cert.pem ./certs/server/private/sv_key.pem
