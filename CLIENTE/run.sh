464be92084b9d021fbaad698730314a63c0489e2
Jeremias Rodriguez <jeremiasrodriguez05@gmail.com>
2025-08-06 23:09:48 -0300

    Update run.sh

--- CLIENTE/run.sh
+++ CLIENTE/run.sh
@@ -1,2 +1,4 @@
-make;
-./clienteTLS localhost:12345 ./certs/ca/ca_cert.pem ./certs/cliente/cl_cert.pem ./certs/cliente/private/cl_key.pem ./certs/cliente/public/cl_pub_key.pem ./external_pub_key/cl_pub_key.pem 
+#!/bin/bash
+read -p "Ingrese el HOST:PUERTO (ejemplo, 127.0.0.1:9999): " HOSTPUERTO
+make
+./clienteTLS "$HOSTPUERTO" ./certs/ca/ca_cert.pem ./certs/cliente/cl_cert.pem ./certs/cliente/private/cl_key.pem ./certs/cliente/public/cl_pub_key.pem ./external_pub_key/cl_pub_key.pem 
